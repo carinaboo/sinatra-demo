932d8b709a73ac6f7a23bb511a34c13d714bea4e
Carina Boo <carinaboo@berkeley.edu>
2017-06-27 21:31:04 -0700

    Final version of demo.

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
+enable :sessions
+
 get '/' do
+	if session[:username]
+		@name = session[:username]
+	else
+		@name = 'Carina'
+	end
 	erb :index
+end
+
+post '/user' do
+	@name = params[:username]
+	session[:username] = @name
+	redirect '/'
 end
